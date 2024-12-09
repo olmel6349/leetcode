10f33748ff85aa050beaabea3597588d58444879
olmel6349 <olmel6349@usn.no>
2024-12-09 07:33:40 +0100

    insert_sort

--- sort-list.py
+++ sort-list.py
@@ -6,7 +6,18 @@ head = [4,2,1,3]
 #head = [-1,5,3,4,0]
 #Output: [-1,0,3,4,5]
 
-def sort(head):
+def insert_sort(head):
+    for i in range(1, len(head)):
+        temp = head[i]
+        j = i-1
+        
+        while j >= 0 and head[j] > temp:
+            head[j+1] = head[j]
+            j -= 1
+            
+        head[j+1] = temp
+
+def bubble_sort(head):
     for i in range(len(head) - 1):
         for j in range(len(head) - i - 1):
             if head[j] > head[j + 1]:
@@ -14,5 +25,5 @@ def sort(head):
                 head[j] = head[j + 1]
                 head[j + 1] = temp
                 
-sort(head)
+insert_sort(head)
 print(head)
